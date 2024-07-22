078c7b21dc07879b15a598e38494d605eac980a2
misheechkin <mishechkin.nikita@mail.ru>
2024-07-22 14:16:49 +0700

    Добавил makefile для сборки динамической библиотеки

---
+++ Module2/6/6.2/list.c
@@ -0,0 +1,120 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "list.h"
+
+int id = 0;
+
+Node *insert(Node *head, Contact contact)
+{
+    Node *new_item;
+    Node *tmp = head;
+    new_item = malloc(sizeof(Node));
+    new_item->contact = contact;
+    if (head == NULL)
+    {
+        new_item->prev = new_item;
+        new_item->next = new_item;
+        head = new_item;
+        id++;
+        new_item->contact.id = id;
+        return head;
+    }
+    do
+    {
+        if (contact_cmp(tmp->contact, new_item->contact) >= 0)
+        {
+            new_item->next = tmp;
+            new_item->prev = tmp->prev;
+            tmp->prev->next = new_item;
+            tmp->prev = new_item;
+            if (tmp == head)
+                head = new_item;
+            id++;
+            new_item->contact.id = id;
+            return head;
+        }
+        tmp = tmp->next;
+    } while (tmp != head);
+    new_item->next = tmp;
+    new_item->prev = tmp->prev;
+    tmp->prev->next = new_item;
+    tmp->prev = new_item;
+    id++;
+    new_item->contact.id = id;
+    return head;
+}
+Node *delete_contact(Node *head, int id)
+{
+    Node *tmp = head;
+    if (NULL == head)
+    {
+        printf("Список пуст!\n");
+        return head;
+    }
+    do
+    {
+        if (id == tmp->contact.id)
+        {
+            if (tmp->next == tmp->prev)
+            {
+                free(tmp);
+                return NULL;
+            }
+            tmp->next->prev = tmp->prev;
+            tmp->prev->next = tmp->next;
+            if (tmp == head)
+                head = head->next;
+            free(tmp);
+            return head;
+        }
+        tmp = tmp->next;
+    } while (tmp != head);
+    printf("В списке нет контакта c таким id!\n");
+    return head;
+}
+
+Contact *find_contact(Node *head, int index)
+{
+    Node *tmp = head;
+    do
+    {
+        if (index == tmp->contact.id)
+            return &tmp->contact;
+        tmp = tmp->next;
+    } while (tmp != head);
+     printf("В списке нет контакта c таким id!\n");
+     return NULL;
+}
+
+Node *delete_list(Node *head)
+{
+    Node *tmp = head;
+    Node *item_remove;
+    if (NULL == head)
+    {
+        return NULL;
+    }
+    do
+    {
+        item_remove = tmp;
+        tmp = tmp->next;
+        free(item_remove);
+    } while (tmp != head);
+    return NULL;
+}
+
+void print_list(Node *head)
+{
+    Node *tmp = head;
+    if (NULL == head)
+    {
+        printf("Список пуст\n");
+        return;
+    }
+    do
+    {
+        print_contact(&(tmp->contact));
+        tmp = tmp->next;
+    } while (tmp != head);
+    printf("\n");
+}
---
+++ Module2/6/6.2/list.h
@@ -0,0 +1,23 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include "contact.h"
+
+typedef struct Node
+{
+    Contact contact;
+    struct Node *next;
+    struct Node *prev;
+} Node;
+
+Node *insert(Node *head, Contact contact);
+
+Node *delete_contact(Node *head, int id);
+
+Node *delete_list(Node *head);
+
+void print_list(Node *head);
+
+Contact *find_contact(Node *head, int index);
+
+#endif
