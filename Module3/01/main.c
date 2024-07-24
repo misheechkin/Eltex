4c1f6fe153a965886269180ade1bbfdec6f75ebd
misheechkin <mishechkin.nikita@mail.ru>
2024-07-24 20:56:19 +0700

    Добавил функцию вычисления площади квадрата

--- Module3/01/main.c
+++ Module3/01/main.c
@@ -1,7 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
 
-
+int calculate_square(int length){
+    return length*length;
+}
 
 
 int main(int argc, char *argv[]){
+    pid_t  pid;
 
+    switch (pid=fork())
+    {
+    case -1:
+        perror("fork");
+        exit(EXIT_FAILURE);
+    case 0: 
+
+    default:
+        break;
+    }
+    exit(EXIT_SUCCESS);
 }
