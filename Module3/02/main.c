466645f20b9f99a807ef01bb5654413039f4b131
misheechkin <mishechkin.nikita@mail.ru>
2024-07-25 02:05:41 +0700

    Добавил парсинг строки

--- Module3/02/main.c
+++ Module3/02/main.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+#define MAX_LENGTH_STRING 50
+#define MAX_AMOUNT_ARGS 10
 
 
 int main(){
-    
+    char input_string[MAX_LENGTH_STRING]={0};
+    for(;;) {
+        printf(">> ");
+        if (fgets(input_string, MAX_LENGTH_STRING, stdin) != NULL) {
+            if (input_string[strlen(input_string) - 1] == '\n') {
+                input_string[strlen(input_string) - 1] = '\0';
+            }
+        }
+        if (strcmp(input_string, "q") == 0) {
+            break;
+        }
+        char *arg = strtok(input_string," ");
+        char *args[MAX_AMOUNT_ARGS]={NULL};
+        for(int i = 0; i < MAX_AMOUNT_ARGS && arg!=NULL; i++) {
+            args[i] = arg;
+            arg = strtok(NULL," ");
+        }
+      
+    }
     exit(EXIT_SUCCESS);
 }
