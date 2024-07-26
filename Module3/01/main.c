8d993fb10a22ab839a40038660d7145387995919
misheechkin <mishechkin.nikita@mail.ru>
2024-07-26 14:05:06 +0700

    Заменил статический массив на динамический

--- Module3/01/main.c
+++ Module3/01/main.c
@@ -14,18 +14,24 @@ int main(int argc, char *argv[])
         printf("Не правильно введены аргументы, введите: <длина стороны квадрата> ... ...\n");
         exit(EXIT_FAILURE);
     }
+    int *side_lengths = NULL;
 
-    int side_lengths[argc-1];
+    if(!(side_lengths = (int*) malloc(argc-1 * sizeof(int)))){
+        printf("Ошибка выделения памяти");
+        exit(EXIT_FAILURE);
+    }
+
     for (int i = 0; i < argc - 1; i++) {
         side_lengths[i] = atoi(argv[i+1]);
     }
 
-    int half = sizeof(side_lengths)/sizeof(*side_lengths)/2;
+    int half = (argc-1)/2;
     pid_t pid;
 
     switch (pid = fork()) {
     case -1:
         perror("fork");
+        free(side_lengths);
         exit(EXIT_FAILURE);
     case 0:
         printf("Процесс-потомок: \n");
@@ -34,13 +40,12 @@ int main(int argc, char *argv[])
         }
         break;
     default:
-        wait(NULL);
         printf("Процесс-родитель: \n");
-        for(int i = half; i < sizeof(side_lengths)/sizeof(*side_lengths); i++) {
+        for(int i = half; i < argc - 1; i++) {
             printf("Площадь квадрата №%d: %d\n", i+1, calculate_square(side_lengths[i]));
         }
         break;
     }
-    
+    free(side_lengths);
     exit(EXIT_SUCCESS);
 }
