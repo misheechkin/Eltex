014d2ea4cbd6d4f7dfd0d1317abc77c093c8ca68
misheechkin <mishechkin.nikita@mail.ru>
2024-07-27 22:48:21 +0700

    Добавил функцию чтения из файла

--- Module3/03/file.c
+++ Module3/03/file.c
@@ -6,7 +6,29 @@
 #include <fcntl.h>
 #include "include/file.h"
 
-
+Node *read_file()
+{
+    Node *head = NULL;
+    int fd = 0;
+    if ((fd = open("contacts_list.bin", O_CREAT | O_RDONLY, 0700)) < 0)
+    {
+        perror("open");
+        exit(EXIT_FAILURE);
+    }
+    int res = -1;
+    Contact contact;
+    while ((res = read(fd, &contact, sizeof(Contact))) != 0)
+    {
+        if (res < 0)
+        {
+            perror("read");
+            close(fd);
+            exit(EXIT_FAILURE);
+        }
+        head = insert_from_file(head, contact);
+    }
+    return head;
+}
 
 void save_file(Node *head)
 {
