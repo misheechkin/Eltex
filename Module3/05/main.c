a281bbf88ac1302ccdf66b167ab269778c3c7929
misheechkin <mishechkin.nikita@mail.ru>
2024-07-30 13:17:22 +0700

    Добавил родителю:  чтение канала и запись в файл

--- Module3/05/main.c
+++ Module3/05/main.c
@@ -37,7 +37,42 @@ int main(int argc, char *argv[]){
     default:
         close(fd[1]);
       
-       
+        int number2 = 0;
+        int res;
+        int temp = 1;
+        char mode[2];
+        while ((res = read(fd[0],&number2,sizeof(number))) != 0)
+        {
+            sleep(1);
+            kill(pid,SIGUSR1);
+            if(res < 0)
+            {
+                perror("read");
+                close(fd[0]);
+                exit(EXIT_FAILURE);
+            }
+            FILE* fdtxt;
+            if(temp)
+            {
+                mode[0]='w';
+                mode[1]='\0';
+                temp = 0 ;
+            }
+            else{
+                mode[0]='a';
+                mode[1]='\0';
+            }
+            if((fdtxt = fopen("numbers.txt", mode)) == NULL)
+            {
+                printf("Не удалось открыть файл");
+                close(fd[0]);
+                exit(EXIT_FAILURE);
+            }   
+            fprintf(fdtxt, "%d\n", number2);
+            fclose(fdtxt); 
+            kill(pid,SIGUSR2);
+        }
+        close(fd[0]);
         break;
     }
     exit(EXIT_SUCCESS);
