57e28dfdb8232eac0cdcad011a8e7ce78785090d
misheechkin <mishechkin.nikita@mail.ru>
2024-08-01 19:44:57 +0700

    Начальный коммит 10

---
+++ Module3/10/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <time.h>
+#include <semaphore.h>
+
+#define SEM_NAME_1 "/my_sem1"
+#define SEM_NAME_2 "/my_sem2"
+
+
+
+int main(int argc, char *argv[]){
+
+    exit(EXIT_SUCCESS);
+}
+
+
