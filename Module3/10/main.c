507c7267802e224c1ca06199d3aced68a99a190e
misheechkin <mishechkin.nikita@mail.ru>
2024-08-01 19:52:53 +0700

    Добавил создание семафоров

--- Module3/10/main.c
+++ Module3/10/main.c
@@ -26,7 +26,30 @@ int count_numbers(int n)
 }
 
 int main(int argc, char *argv[]){
-
+    if (argc == 1 || argc > 2) {
+         printf("Не правильно введен аргумент, введите: <количество случайных чисел>\n");
+         exit(EXIT_FAILURE);
+    }
+    int amount = atoi(argv[1]);
+    int fd[2];
+    sem_t* semaphore1;
+    sem_t* semaphore2;
+    
+    if((semaphore1 = sem_open(SEM_NAME_1,O_CREAT,0600,0)) == SEM_FAILED)
+    {
+        perror("sem_open");
+        exit(EXIT_FAILURE);
+    }
+    if((semaphore2 = sem_open(SEM_NAME_2,O_CREAT,0600,1)) == SEM_FAILED)
+    {
+        perror("sem_open");
+        exit(EXIT_FAILURE);
+    }
+    if (pipe(fd) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+   
     exit(EXIT_SUCCESS);
 }
 
