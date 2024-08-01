246b639e87d93ae4ced2627aff5d9b7fb7acccc8
misheechkin <mishechkin.nikita@mail.ru>
2024-08-01 19:47:21 +0700

    Добавил функцию для расчета байт в числе

--- Module3/10/main.c
+++ Module3/10/main.c
@@ -9,7 +9,21 @@
 #define SEM_NAME_1 "/my_sem1"
 #define SEM_NAME_2 "/my_sem2"
 
-
+int count_numbers(int n)
+{
+    int numbers;
+    if (n/10==0)
+      return 1;
+    else
+    {
+        while (n>0) 
+        {  
+            n=n/10;
+            numbers+=1;
+        }
+        return numbers;
+    }     
+}
 
 int main(int argc, char *argv[]){
 
