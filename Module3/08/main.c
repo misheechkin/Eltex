e20d5a4d687d69c6eac0770a1320400f29c90a77
misheechkin <mishechkin.nikita@mail.ru>
2024-08-02 22:47:26 +0700

    Добавил создание семафора

--- Module3/08/main.c
+++ Module3/08/main.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <time.h>
-#include <semaphore.h>
+#include <sys/sem.h>
 
 #define SEM_NAME_1 "/my_sem1"
 #define SEM_NAME_2 "/my_sem2"
@@ -33,19 +33,19 @@ int main(int argc, char *argv[]){
     }
     int amount = atoi(argv[1]);
     int fd[2];
-    sem_t* semaphore1;
-    sem_t* semaphore2;
-    
-    if((semaphore1 = sem_open(SEM_NAME_1,O_CREAT,0600,0)) == SEM_FAILED)
-    {
-        perror("sem_open");
+
+    key_t key;
+    int semid;
+    if((key = ftok("./main.c", 'E'))== -1){
+        perror("ftok");
         exit(EXIT_FAILURE);
     }
-    if((semaphore2 = sem_open(SEM_NAME_2,O_CREAT,0600,1)) == SEM_FAILED)
-    {
-        perror("sem_open");
+    if((semid = semget(key, 10, 0666 | IPC_CREAT))== -1){
+        perror("ftok");
         exit(EXIT_FAILURE);
     }
+
+
     if (pipe(fd) == -1) {
         perror("pipe");
         exit(EXIT_FAILURE);
@@ -71,11 +71,11 @@ int main(int argc, char *argv[]){
                 close(fd[1]);
                 exit(EXIT_FAILURE);
             }
-            if(sem_wait(semaphore1) != 0)
-            {
-                perror("sem_close");
-                exit(EXIT_FAILURE);
-            }
+            // if(sem_wait(semaphore1) != 0)
+            // {
+            //     perror("sem_close");
+            //     exit(EXIT_FAILURE);
+            // }
            
             FILE* fdtxt1;
             if((fdtxt1 = fopen("numbers.txt", "r")) == NULL)
@@ -89,13 +89,13 @@ int main(int argc, char *argv[]){
             fclose(fdtxt1);
             number_of_digits += count_numbers(number)+1;
         }
-        sem_close(semaphore1);
+        // sem_close(semaphore1);
         close(fd[1]);
-        if(sem_unlink(SEM_NAME_1) != 0)
-        {
-            perror("sem_unlink");
-            exit(EXIT_FAILURE);
-        }
+        // if(sem_unlink(SEM_NAME_1) != 0)
+        // {
+        //     perror("sem_unlink");
+        //     exit(EXIT_FAILURE);
+        // }
        break;
     default:
         close(fd[1]);
@@ -105,11 +105,11 @@ int main(int argc, char *argv[]){
         char mode[2];
         while ((res = read(fd[0],&number2,sizeof(number))) != 0)
         {
-            if(sem_wait(semaphore2)!= 0)
-            {
-                perror("sem_close");
-                exit(EXIT_FAILURE);
-            }
+            // if(sem_wait(semaphore2)!= 0)
+            // {
+            //     perror("sem_close");
+            //     exit(EXIT_FAILURE);
+            // }
            
             if(res < 0)
             {
@@ -137,31 +137,31 @@ int main(int argc, char *argv[]){
             }   
             fprintf(fdtxt, "%d\n", number2);
             fclose(fdtxt); 
-            if (sem_post(semaphore1) != 0){
-                perror("sem_post");
-                exit(EXIT_FAILURE);
-            }   
-            if (sem_post(semaphore2)!= 0){
-                perror("sem_post");
-                exit(EXIT_FAILURE);
-            }
+            // if (sem_post(semaphore1) != 0){
+            //     perror("sem_post");
+            //     exit(EXIT_FAILURE);
+            // }   
+            // if (sem_post(semaphore2)!= 0){
+            //     perror("sem_post");
+            //     exit(EXIT_FAILURE);
+            // }
         }
-        if(sem_close(semaphore1)!= 0)
-        {
-            perror("sem_close");
-            exit(EXIT_FAILURE);
-        }
-        if(sem_close(semaphore2) != 0)
-        {
-            perror("sem_close");
-            exit(EXIT_FAILURE);
-        }
-        if(sem_unlink(SEM_NAME_2) != 0)
-        {
-            perror("sem_unlink1");
-            exit(EXIT_FAILURE);
-        }
-        close(fd[0]);
+        // if(sem_close(semaphore1)!= 0)
+        // {
+        //     perror("sem_close");
+        //     exit(EXIT_FAILURE);
+        // }
+        // if(sem_close(semaphore2) != 0)
+        // {
+        //     perror("sem_close");
+        //     exit(EXIT_FAILURE);
+        // }
+        // if(sem_unlink(SEM_NAME_2) != 0)
+        // {
+        //     perror("sem_unlink1");
+        //     exit(EXIT_FAILURE);
+        // }
+        // close(fd[0]);
         break;
     }
     exit(EXIT_SUCCESS);
