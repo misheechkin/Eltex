d6817a1d8ad4a63b83eaaf00048fbf43b31d65f9
misheechkin <mishechkin.nikita@mail.ru>
2024-08-10 14:27:23 +0700

    Начальный коммит 14

---
+++ Module3/15/client.c
@@ -0,0 +1,13 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[]) {
+}
